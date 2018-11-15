e3872fc2b05eeb575399debbd86ad8190248901a
Sean Thomas <thomsean@mail.gvsu.edu>
2018-11-15 16:05:22 -0500

    11/15/2018 added a few things. lots of errors.

--- app/src/main/java/com/example/rosa/finderseekers/MainActivity.java
+++ app/src/main/java/com/example/rosa/finderseekers/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.rosa.finderseekers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+//https://developers.facebook.com/apps/347284212501597/fb-login/quickstart/
 public class MainActivity extends AppCompatActivity {
 
+    callbackManager = CallbackManager.Factory.create();
+
+    //Use this with the button listener
+    //LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private static final String EMAIL = "email";
+
+    loginButton = (LoginButton) findViewById(R.id.login_button);
+    loginButton.setReadPermissions(Arrays.asList(EMAIL));
+    // If you are using in a fragment, call loginButton.setFragment(this);
+
+    // Callback registration
+    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            // App code
+        }
+
+        @Override
+        public void onCancel() {
+            // App code
+        }
+
+        @Override
+        public void onError(FacebookException exception) {
+            // App code
+        }
+    });
+    //logger.logPurchase(BigDecimal.valueOf(4.32), Currency.getInstance("USD")); probably wont need
+
 }
