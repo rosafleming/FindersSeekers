d23cf3fdd7271ac40089cbb9681f8479e9b87013
cgonzalez74 <gonzalezch74@gmail.com>
2018-12-09 19:27:11 -0500

    Added Seekers

--- app/src/main/java/com/example/rosa/finderseekers/SeekersActivity.java
+++ app/src/main/java/com/example/rosa/finderseekers/SeekersActivity.java
@@ -1,5 +1,7 @@
 package com.example.rosa.finderseekers;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -25,6 +27,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 public class SeekersActivity extends BaseActivity {
 
     private Spinner state, city;
@@ -32,7 +36,10 @@ public class SeekersActivity extends BaseActivity {
     DatabaseReference topRef;
     DatabaseReference users;
     ListView listView;
-
+    private String currentState;
+    private String selected_city;
+    private ArrayAdapter<String> dataAdapter;
+    private List<String> city_list;
 
     @Override
     int getContentViewId() {
@@ -49,12 +56,48 @@ public class SeekersActivity extends BaseActivity {
         Log.i("Clicked","Seekers Open");
         Log.i("USER", MainActivity.EMAIL);
 
+        currentState = "";
+        selected_city = "";
+        city = (Spinner) findViewById(R.id.spinner2);
+
+        addItemsOnSpinner2();
         addListenerOnButton();
         addListenerOnSpinnerItemSelection();
-        addItemsOnSpinner2();
-        listView = (ListView) findViewById(R.id.seekerSpotList);
 
         topRef = FirebaseDatabase.getInstance().getReference();
+        listView = (ListView) findViewById(R.id.seekerSpotList);
+
+        users = topRef.child("Users");
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+
+                    if((Integer.parseInt(ds.getValue(ProfileContent.ContentItem.class).getSeekersNum()) - Integer.parseInt(ds.getValue(ProfileContent.ContentItem.class).getFindersNum())) > 5) {
+                        AlertDialog alertDialog = new AlertDialog.Builder(SeekersActivity.this).create();
+                        alertDialog.setTitle("Alert");
+                        alertDialog.setMessage("Please Add More Spots Before Seeking.");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        Intent intent = new Intent(SeekersActivity.this, FindersActivity.class);
+                                        startActivity (intent);
+                                    }
+                                });
+                        alertDialog.show();
+
+                    }
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("FindData", "onCancelled", databaseError.toException());
+            }
+        });
+
         DatabaseReference spots = topRef.child("Spots");
 
         spots.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -101,6 +144,7 @@ public class SeekersActivity extends BaseActivity {
                     public void onDataChange(DataSnapshot dataSnapshot) {
 
                         for(DataSnapshot ds : dataSnapshot.getChildren()){
+
                             if(ds.getValue(ProfileContent.ContentItem.class).getEmail().equals(MainActivity.EMAIL)) {
                                 int seekerCounter = Integer.parseInt(ds.getValue(ProfileContent.ContentItem.class).getSeekersNum());
                                 seekerCounter=seekerCounter+1;
@@ -128,8 +172,7 @@ public class SeekersActivity extends BaseActivity {
     // add items into spinner dynamically
     public void addItemsOnSpinner2() {
 
-        city = (Spinner) findViewById(R.id.spinner2);
-        List<String> list = new ArrayList<String>();
+        city_list = new ArrayList<String>();
 
         topRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference cities = topRef.child("Spots");
@@ -138,22 +181,22 @@ public class SeekersActivity extends BaseActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 //SpotContent.ITEMS.clear();
+                Boolean cityExists = false;
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
                     if(ds.getValue(SpotContent.SpotItem.class).getState().equals(String.valueOf(state.getSelectedItem()))){
-                        list.add(ds.getValue(SpotContent.SpotItem.class).getCity());
+                        for(int i = 0; i < city_list.size(); i++) {
+                            if(city_list.get(i).equals(ds.getValue(SpotContent.SpotItem.class).getCity())){
+                                cityExists = true;
+                            }
+                        }
+                        if(cityExists == true){
+                            cityExists = false;
+                        }else{
+                            city_list.add(ds.getValue(SpotContent.SpotItem.class).getCity());
+                        }
                     }
 
                 }
-                String[] SpotList = new String[SpotContent.ITEMS.size()];
-
-                for (int i = 0; i < SpotContent.ITEMS.size(); i++){
-                    SpotList[i] = SpotContent.ITEMS.get(i).toString();
-                }
-
-                ArrayAdapter<String> itemsAdapter =
-                        new ArrayAdapter<String>(SeekersActivity.this, android.R.layout.simple_list_item_1,SpotList );
-
-                listView.setAdapter(itemsAdapter);
 
             }
             @Override
@@ -162,12 +205,12 @@ public class SeekersActivity extends BaseActivity {
             }
         });
 
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
+        dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, city_list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         city.setAdapter(dataAdapter);
-        city.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+
+        //city.setSelection(0);
     }
 
     public void addListenerOnSpinnerItemSelection() {
@@ -175,7 +218,59 @@ public class SeekersActivity extends BaseActivity {
         state.setOnItemSelectedListener(new CustomOnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                addItemsOnSpinner2();
+                if(!String.valueOf(state.getSelectedItem()).equals(currentState)){
+                    Log.i("STATE_SWITCH", "Ran This");
+
+
+                    topRef = FirebaseDatabase.getInstance().getReference();
+                    DatabaseReference cities = topRef.child("Spots");
+
+                    cities.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            city_list.clear();
+                            Boolean cityExists = false;
+                            for(DataSnapshot ds : dataSnapshot.getChildren()){
+                                if(ds.getValue(SpotContent.SpotItem.class).getState().equals(String.valueOf(state.getSelectedItem()))){
+                                    for(int i = 0; i < city_list.size(); i++) {
+                                        if(city_list.get(i).equals(ds.getValue(SpotContent.SpotItem.class).getCity())){
+                                            cityExists = true;
+                                        }
+                                    }
+                                    if(cityExists == true){
+                                        cityExists = false;
+                                    }else{
+                                        city_list.add(ds.getValue(SpotContent.SpotItem.class).getCity());
+                                    }
+                                }
+                            }
+
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Log.e("FindData", "onCancelled", databaseError.toException());
+                        }
+                    });
+
+                    dataAdapter.notifyDataSetChanged();
+                    city.setSelection(0);
+                    Log.i("STATE_SWITCH", "Notified");
+
+                    city.setOnItemSelectedListener(new CustomOnItemSelectedListener(){
+                        @Override
+                        public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                            selected_city = String.valueOf(city.getSelectedItem());
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parentView) {
+                            // your code here
+                        }
+                    });
+
+                    currentState = String.valueOf(state.getSelectedItem());
+                }
+
             }
 
             @Override
@@ -188,8 +283,8 @@ public class SeekersActivity extends BaseActivity {
     // get the selected dropdown list value
     public void addListenerOnButton() {
 
-        //state = (Spinner) findViewById(R.id.spinner1);
-        //city = (Spinner) findViewById(R.id.spinner2);
+        state = (Spinner) findViewById(R.id.spinner1);
+        city = (Spinner) findViewById(R.id.spinner2);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -198,6 +293,7 @@ public class SeekersActivity extends BaseActivity {
             public void onClick(View v) {
 
                 DatabaseReference newList = topRef.child("Spots");
+                Log.i("STATE_CITY", String.valueOf(state.getSelectedItem()) + " and " + selected_city);
 
                 newList.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
@@ -205,6 +301,7 @@ public class SeekersActivity extends BaseActivity {
                         SpotContent.ITEMS.clear();
 
                         for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
                             if(ds.getValue(SpotContent.SpotItem.class).getState().equals(String.valueOf(state.getSelectedItem()))&& ds.getValue(SpotContent.SpotItem.class).getCity().equals(city.getSelectedItem()))
                             {
                                 SpotContent.SpotItem item = new SpotContent.SpotItem(
